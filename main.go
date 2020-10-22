e4458d60320ed7bb1eaa65ce02fdb4fdf7f6e86c
Alex W <42474028+phtechwohletz@users.noreply.github.com>
2020-10-22 15:59:51 -0700

    Update main.go
    Correct the messaging.

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World, Alex is the Best."}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
